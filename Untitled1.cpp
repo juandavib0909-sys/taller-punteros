b9599150fd5fb371ba1135f45a7543ae8306d1ad
juandavib0909-sys <juandavib0909@gmail.com>
2026-04-14 08:43:42 -0500

    Add files via upload

--- Untitled1.cpp
+++ Untitled1.cpp
@@ -1,83 +1,41 @@
 #include <iostream>
 using namespace std;
 
-int** crearMatriz(int n, int m) {
-    int** mat = new int*[n];
+double cuadrado(double x) { return x * x; }
+double doble(double x) { return 2 * x; }
+double inverso(double x) { return (x != 0) ? 1 / x : 0; }
+
+void aplicar(double* arr, int n, double (*func)(double)) {
     for (int i = 0; i < n; i++)
-        mat[i] = new int[m];
-    return mat;
+        arr[i] = func(arr[i]);
 }
 
-void llenarMatriz(int** mat, int n, int m) {
-    for (int i = 0; i < n; i++)
-        for (int j = 0; j < m; j++) {
-            cout << "Elemento [" << i << "][" << j << "]: ";
-            cin >> mat[i][j];
-        }
+int main(){
+int n;
+cout << "Tamano del arreglo: ";
+cin >> n;
+
+double* arr = new double[n];
+
+for (int i = 0; i < n; i++) {
+    cout << "Elemento " << i << ": ";
+    cin >> arr[i];
 }
 
-void sumaFilas(int** mat, int n, int m) {
-    for (int i = 0; i < n; i++) {
-        int suma = 0;
-        for (int j = 0; j < m; j++)
-            suma += mat[i][j];
-        cout << "Suma fila " << i << ": " << suma << endl;
-    }
+int opcion;
+cout << "1. Cuadrado\n2. Doble\n3. Inverso\nOpcion: ";
+cin >> opcion;
+
+switch (opcion) {
+    case 1: aplicar(arr, n, cuadrado); break;
+    case 2: aplicar(arr, n, doble); break;
+    case 3: aplicar(arr, n, inverso); break;
 }
 
-int mayorElemento(int** mat, int n, int m) {
-    int mayor = mat[0][0];
-    for (int i = 0; i < n; i++)
-        for (int j = 0; j < m; j++)
-            if (mat[i][j] > mayor)
-                mayor = mat[i][j];
-    return mayor;
-}
+for (int i = 0; i < n; i++)
+    cout << arr[i] << " ";
 
-int** transponer(int** mat, int n, int m) {
-    int** t = crearMatriz(m, n);
-    for (int i = 0; i < n; i++)
-        for (int j = 0; j < m; j++)
-            t[j][i] = mat[i][j];
-    return t;
-}
+delete[] arr;
+return 0;
 
-void liberar(int** mat, int n) {
-    for (int i = 0; i < n; i++)
-        delete[] mat[i];
-    delete[] mat;
-}
-
-int main() {
-    int n, m;
-
-    cout << "Filas: ";
-    cin >> n;
-    cout << "Columnas: ";
-    cin >> m;
-
-    int** matriz = crearMatriz(n, m);
-
-    llenarMatriz(matriz, n, m);
-
-    cout << "\n--- Suma de filas ---\n";
-    sumaFilas(matriz, n, m);
-
-    int mayor = mayorElemento(matriz, n, m);
-    cout << "\nMayor elemento: " << mayor << endl;
-
-    int** transpuesta = transponer(matriz, n, m);
-
-    cout << "\n--- Matriz Transpuesta ---\n";
-    for (int i = 0; i < m; i++) {
-        for (int j = 0; j < n; j++) {
-            cout << transpuesta[i][j] << " ";
-        }
-        cout << endl;
-    }
-
-    liberar(matriz, n);
-    liberar(transpuesta, m);
-
-    return 0;
 }
